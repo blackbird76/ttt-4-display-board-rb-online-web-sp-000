11976459635353fac301fbac98a932547355cd05
Megan McCullough <mmccullough206@gmail.com>
2019-03-11 22:14:04 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,4 @@
-board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+board = ["X", "X", "X", " ", " ", "N", " ", " ", " "]
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
